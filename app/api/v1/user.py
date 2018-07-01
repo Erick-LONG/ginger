8ad3ef27e5ab7a03c42d8d14fa3d6db9af37879c
zhangfulong <834424581@qq.com>
2018-07-01 22:09:29 +0800

    构建客户端验证器

--- app/api/v1/user.py
+++ app/api/v1/user.py
@@ -5,6 +5,13 @@ from app.libs.redprint import Redprint
 
 api = Redprint("user")
 
+
 @api.route('/get')
 def get_user():
+    return 'i am hah'
+
+
+@api.route('/create')
+def create_user():
+    #name,pwd
     return 'i am hah'
